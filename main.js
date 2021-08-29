9c9b62c538a7697486bf59d5f33416d5a0a954f8
KieceDonc <valentin.verstracte@gmail.com>
2021-08-29 23:36:01 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,54 @@
+const bot = new Discord.Client();
+const secret = require("./secret.js");
+
+
+Bot.on('message', async message => {
+  
+  let moduleName = message.content;
+  let currentServObject = message.guild;
+
+  if(message.channel.id=="881644144869650492"){
+    var currentModuleRoleID = createRole(currentServObject,moduleName);
+    /*try{
+        createCategory(currentServObject,moduleName).then(()=>{
+            createAllChannels(currentServObject,moduleName).then(()=>{
+                message.channel.send(MC+" done");
+            })
+        })
+    }catch(err){
+        message.channel.send("err:\n"+err);
+    }
+  }*/
+});
+
+function createRole(currentServObject,moduleName){
+    currentServObject.roles.create({
+        name:moduleName, 
+        color: "#9e9e9e", 
+        hoist: false,
+        mentionable: true
+    }).then(console.log);
+}
+
+function createCategory(currentServObject,moduleName){
+    return new Promise((resolve)=>{
+        currentServObject.channels.create(moduleName, {
+            type: 'category',
+            permissionOverwrites: [
+                {
+                    id: message.guild.id,
+                    allow: ['VIEW_CHANNEL'],
+                }]
+        }).then(()=>{
+            resolve;
+        })
+    });
+}
+
+function createAllChannels(currentServObject,moduleName){
+    return new Promise((resolve)=>{
+        resolve;
+    })
+}
+
+bot.login(secret.token);
