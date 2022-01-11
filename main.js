9b36e86d3f499a854f0096e0c3bb0c0bf7f09256
KieceDonc <valentinverst.developer@gmail.com>
2022-01-11 17:41:09 +0100

    Update to last version of discord.js

--- main.js
+++ main.js
@@ -1,21 +1,24 @@
-const Discord = require('discord.js')
-const bot = new Discord.Client();
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS,Intents.FLAGS.GUILD_MESSAGES] });
 const secret = require("./secret.js");
 
 
-bot.on('message', async message => {
-  
+var allChanCurrentSemestreID = "930490087878299688";
+
+client.on('message', async message => {
+ 
     var authorID = message.author.id;
-    var moduleName = message.content;
+    var moduleName = message.content.split("&")[0];
+    var moduleAbbreviation = message.content.split("&")[1];
     var currentServObject = message.guild;
 
-    if(message.channel.id=="881639490878332938" && authorID!="881642877984317452"){
+    if(message.channel.id=="881639490878332938" && authorID=="302529594685128707"){
         try{
             createRole(currentServObject,moduleName).then((currentRoleID)=>{
-                createCategory(currentServObject,moduleName,currentRoleID).then((currentCategory)=>{
+                createCategory(currentServObject,moduleName).then((currentCategory)=>{
                     changeCategoryPerm(currentServObject,currentCategory,currentRoleID).then(()=>{
-                        createAllChannels(currentServObject,currentCategory.id).then(()=>{
-                            message.channel.send(message.content+" done");
+                        createAllChannels(currentServObject,currentCategory.id,moduleAbbreviation).then(()=>{
+                            message.channel.send("Module "+moduleName+" avec abrévation "+moduleAbbreviation+" finit");
                         })
                     })
                 })
@@ -27,47 +30,44 @@ bot.on('message', async message => {
 });
 
 function createRole(currentServObject,moduleName){
-    return new Promise((resolve)=>{ 
+    return new Promise((resolve)=>{
         // creating role for current module
         currentServObject.roles.create({
-            data: {
-                name: moduleName, 
-                color: "#9e9e9e", 
-                mentionable: true,
-              },
+            name: moduleName,
+            color: "#9e9e9e",
             reason: 'we needed a role for Super Cool People',
-        }).then((currentRole)=>{
+          }).then((currentRole)=>{
             resolve(currentRole.id)
         });
     })
 }
 
-function createCategory(currentServObject,moduleName,currentRoleID){
-    return new Promise((resolve)=>{ 
+function createCategory(currentServObject,moduleName){
+    return new Promise((resolve)=>{
         // creating category for current module
         currentServObject.channels.create(moduleName, {
-            type: 'CATEGORY',
+            type: 'GUILD_CATEGORY',
         }).then((currentCategory)=>{
             resolve(currentCategory);
         })
     });
 }
 
-function createAllChannels(currentServObject,currentCategoryID){
+function createAllChannels(currentServObject,currentCategoryID,moduleAbbreviation){
     return new Promise((resolve)=>{
-        currentServObject.channels.create("annonces-", {
-            type: 'TEXT',
-            parent: currentCategoryID, 
+        currentServObject.channels.create("annonces-"+moduleAbbreviation, {
+            type: 'GUILD_TEXT',
+            parent: currentCategoryID,
         }).then((annoncesChan)=>{
             annoncesChan.lockPermissions();
-            currentServObject.channels.create("général-", {
-                type: 'TEXT',
-                parent: currentCategoryID, 
+            currentServObject.channels.create("général-"+moduleAbbreviation, {
+                type: 'GUILD_TEXT',
+                parent: currentCategoryID,
             }).then((generalChan)=>{
-                annoncesChan.lockPermissions();
-                currentServObject.channels.create("fichiers-", {
-                    type: 'TEXT',
-                    parent: currentCategoryID, 
+                generalChan.lockPermissions();
+                currentServObject.channels.create("fichiers-"+moduleAbbreviation, {
+                    type: 'GUILD_TEXT',
+                    parent: currentCategoryID,
                 }).then((fichiersChan)=>{
                     fichiersChan.lockPermissions();
                     resolve();
@@ -81,13 +81,17 @@ function changeCategoryPerm(currentServObject,currentCategory,currentRoleID){
     return new Promise((resolve)=>{
         var everyoneRoleID = currentServObject.roles.everyone.id;
         // changing permissions to allow only the role of the current module to see it and not everyone
-        currentCategory.overwritePermissions([
+        currentCategory.permissionOverwrites.set([
             {
-                id: currentRoleID, 
+                id: currentRoleID,
                 allow: 'VIEW_CHANNEL',
             },
             {
-                id: everyoneRoleID, 
+                id: allChanCurrentSemestreID,
+                allow: 'VIEW_CHANNEL',
+            },
+            {
+                id: everyoneRoleID,
                 deny: 'VIEW_CHANNEL',
             }
         ]).then(()=>{
@@ -96,4 +100,4 @@ function changeCategoryPerm(currentServObject,currentCategory,currentRoleID){
     })
 }
 
-bot.login(secret.token);
+client.login(secret.token);
